aa4bb3a5ef532b7fc78d835fbfa62b3e2ddf62c0
Shaik Mullasadik <sriskms786@gmail.com>
2026-06-21 23:19:46 +0530

    Fix port binding dynamically using PowerShell script

--- server.ts
+++ server.ts
@@ -7,7 +7,7 @@ import { createServer as createViteServer } from "vite";
 dotenv.config();
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 8080;
 
 app.use(express.json());
 
